8ff9b9a4b347afb23f279aa805780918a82cfd94
Donggyu Kim <ptsaturn68@daum.net>
2021-11-01 21:12:55 +0900

    [FE] style: emotion theme 설정 및 엔트리 적용
    - emotion theme 세팅
    - 엔트리에 적용
    
    - issue (#31)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
+import GlobalStyle from './styles/global';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
