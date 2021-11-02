51992acde92e971292a875588a4eca29a85fe05f
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-02 16:08:03 +0900

    [FE] design: CategoryList css 작성

--- client/src/components/SearchFilter/Header/CategoryList.tsx
+++ client/src/components/SearchFilter/Header/CategoryList.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const dummyData = ['공모전', '프로젝트', '대외활동'];
+const dummyData = [
+  '공모전',
+  '프로젝트',
+  '대외활동',
+  '스터디',
+  '동아리',
+  '면접/인터뷰',
+  '토이프로젝트',
+  '구인/구직',
+  '기타',
+];
 
 function CategoryList(): JSX.Element {
   return (
@@ -15,13 +25,18 @@ function CategoryList(): JSX.Element {
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
   width: 80%;
+  align-items: center;
 `;
 
 const CategoryListItem = styled.div`
   display: flex;
-  margin: 10px;
+  margin: 5px;
+  padding: 10px;
+
+  background: ${(props) => props.theme.Gray4};
+  box-shadow: 20px 20px 40px 4px rgba(41, 36, 36, 0.25);
+  border-radius: 10px;
 `;
 
 export default CategoryList;
