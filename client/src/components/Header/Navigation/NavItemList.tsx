b077a01b3d894f7b843a4c23c29ceeaa5aced7cf
Donggyu Kim <ptsaturn68@daum.net>
2021-11-02 18:14:56 +0900

    [FE] feat: 헤더 네비게이션 컴포넌트 추가
    - issue (#11)

---
+++ client/src/components/Header/Navigation/NavItemList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import NavItem from './NavItem';
+
+interface NavigationItem {
+  title: string;
+  path: string;
+}
+
+function NavItemList(): JSX.Element {
+  const navItemList: NavigationItem[] = [
+    { title: '그룹 탐색', path: 'recruit/group' },
+    { title: '멘토 찾기', path: 'recruit/mentors' },
+    { title: '참여 그룹', path: 'groups' },
+  ];
+
+  const navItems = navItemList.map((item, idx) => {
+    return <NavItem title={item.title} path={item.path} key={idx} />;
+  });
+
+  return <Container>{navItems}</Container>;
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 640px;
+  box-sizing: border-box;
+`;
+
+export default NavItemList;
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Logo from './Logo';
+import Navigation from './Navigation';
 
 function Header(): JSX.Element {
   return (
     <Container>
-      <Logo />
+      <Content>
+        <Logo />
+        <Navigation />
+        {/* 역할 스위치 버튼 */}
+        {/* 알림 버튼 */}
+        {/* 프로필 버튼 */}
+      </Content>
     </Container>
   );
 }
@@ -13,12 +20,24 @@ function Header(): JSX.Element {
 const Container = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
   align-items: center;
   width: 100%;
   height: 80px;
-  padding: 0px 32px 0px 32px;
+  padding: 12px 32px 0px 32px;
   box-sizing: border-box;
   box-shadow: 0 2px 4px 0 hsl(0deg 0% 81% / 50%);
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 75vw;
+  height: 80px;
+  min-width: 800px;
+  box-sizing: border-box;
 `;
 
 export default Header;
