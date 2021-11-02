16f6795fc981f012ad9ca992e723db32e1773734
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-02 14:27:02 +0900

    [FE] design: SearchFilter Header 세부 컴포넌트 생성
    - SearchBar, CategoryList 컴포넌트 생성
    
    - issue (#24)

---
+++ client/src/components/SearchFilter/Header/CategoryList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+function CategoryList(): JSX.Element {
+  return <Container></Container>;
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+`;
+
+export default CategoryList;
---
+++ client/src/components/SearchFilter/SearchBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+function SearchBar(): JSX.Element {
+  return <Container>searchBar</Container>;
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+export default SearchBar;
