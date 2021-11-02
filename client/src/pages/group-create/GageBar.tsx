5841f607e15eef9e3b4941d3eea84b7e61ab0058
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-02 14:24:50 +0900

    [FE] design: GageBar 레이아웃 생성
    - 게이지바 컴포넌트 생성및 기본 레이아웃 구현
    - issue (#22)

---
+++ client/src/pages/group-create/GageBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "@emotion/styled";
+
+function GageBar(): JSX.Element {
+  return(
+    <GageBarContainer>GageBar</GageBarContainer>
+  )
+}
+
+const GageBarContainer = styled.div`
+  text-align: center;
+`
+export default GageBar;
