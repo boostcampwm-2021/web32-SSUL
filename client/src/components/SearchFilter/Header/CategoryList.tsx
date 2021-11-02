07170731e6efa1cc9792b6e9e8b1f4d0d1307670
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-02 14:29:28 +0900

    [FE] feat: CategoryList 더미 데이터 출력
    - Category 더미 데이터 출력
    
    - issue (#24)

--- client/src/components/SearchFilter/Header/CategoryList.tsx
+++ client/src/components/SearchFilter/Header/CategoryList.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+const dummyData = ['공모전', '프로젝트', '대외활동'];
+
 function CategoryList(): JSX.Element {
-  return <Container></Container>;
+  return (
+    <Container>
+      {dummyData.map((category, idx) => {
+        return <CategoryListItem key={idx}>{category}</CategoryListItem>;
+      })}
+    </Container>
+  );
 }
 
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
   width: 80%;
+`;
+
+const CategoryListItem = styled.div`
+  display: flex;
+  margin: 10px;
 `;
 
 export default CategoryList;
