920641b90f83cbb0e108701fd1b6237c5bb8cf02
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-02 14:32:32 +0900

    [FE] feat: TechList 더미 데이터 출력
    - 기술스택 더미 데이터 출력
    
    - issue (#24)

--- client/src/components/SearchFilter/TechSection/TechList.tsx
+++ client/src/components/SearchFilter/TechSection/TechList.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+const dummyData = ['express', 'react', 'node.js', 'test'];
+
 function TechList(): JSX.Element {
-  return <Container></Container>;
+  return (
+    <Container>
+      {dummyData.map((category, idx) => {
+        return <TechListItem key={idx}>{category}</TechListItem>;
+      })}
+    </Container>
+  );
 }
 
 const Container = styled.div`
+  display: flex;
+`;
+
+const TechListItem = styled.div`
   display: flex;
 `;
 
