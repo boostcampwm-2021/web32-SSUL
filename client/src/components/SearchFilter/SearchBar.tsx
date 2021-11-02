b8e04ce659ca326e89388805c0611809f2eb99fc
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-02 16:07:12 +0900

    [FE] design: SearchBar css 작성

--- client/src/components/SearchFilter/SearchBar.tsx
+++ client/src/components/SearchFilter/SearchBar.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 function SearchBar(): JSX.Element {
-  return <Container>searchBar</Container>;
+  return <Container>Saerch for...</Container>;
 }
 
 const Container = styled.div`
   display: flex;
+  margin: 10px;
+  padding: 10px;
+
+  box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 30px 30px 30px 30px;
 `;
 
 export default SearchBar;
