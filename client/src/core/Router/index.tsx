ff3f44e35e368da620d7a245da6a45abc881832f
Donggyu Kim <ptsaturn68@daum.net>
2021-11-03 15:52:02 +0900

    [FE] feat: 기획 페이지 추가 및 라우터 등록
    - 기획에서 만들어진 페이지 url 라우터 등록
    
    - issue (#42)

--- client/src/core/Router/index.tsx
+++ client/src/core/Router/index.tsx
@@ -4,6 +4,12 @@ import MainPage from '../../pages/MainPage';
 import GroupRecruitPage from '../../pages/GroupRecruitPage';
 import MentorRecruitPage from '../../pages/MentorRecruitPage';
 import GroupsPage from '../../pages/GroupsPage';
+import GroupStatusPage from '../../pages/GroupStatusPage';
+import GroupOwnerPage from '../../pages/GroupOwnerPage';
+import EvaluateGroupPage from '../../pages/EvaluateGroupPage';
+import ChatListPage from '../../pages/ChatListPage';
+import ChatPage from '../../pages/ChatPage';
+import ProfilePage from '../../pages/ProfilePage';
 
 function Router(): JSX.Element {
   return (
@@ -12,6 +18,12 @@ function Router(): JSX.Element {
       <Route path="/recruit/group" component={GroupRecruitPage} exact />
       <Route path="/recruit/mentor" component={MentorRecruitPage} exact />
       <Route path="/groups" component={GroupsPage} exact />
+      <Route path="/group/status" component={GroupStatusPage} />
+      <Route path="/group/owner" component={GroupOwnerPage} />
+      <Route path="/group/evaluate" component={EvaluateGroupPage} />
+      <Route path="/chat/list" component={ChatListPage} />
+      <Route path="/chat" component={ChatPage} />
+      <Route path="/profile" component={ProfilePage} />
       <Redirect to="/" />
     </Switch>
   );
---
+++ client/src/pages/EvaluateGroupPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+function EvaluateGroupPage(): JSX.Element {
+  return <Container></Container>;
+}
+
+const Container = styled.div``;
+
+export default EvaluateGroupPage;
