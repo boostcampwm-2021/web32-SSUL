c10ae4b91fbefecf08be507dedd71edf6e53bee5
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-03 15:49:49 +0900

    [FE] feat: Category Contents구현
    - 카테고리 더미파일 테스트(추후 실데이터로 변경 필요)
    - 선택된 카테고리 state 관리
    - 선택된 카테고리 눌림 효과 적용
    - CategoryItem 컴포넌트 분리

---
+++ client/src/pages/CreateGroupPage/Category/CategoryItem.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface ItemProp {
+  key: number,
+  category: string,
+  clicked: boolean,
+  clickCategory :() => void
+}
+
+function CategoryItem({category, clicked, clickCategory}: ItemProp): JSX.Element {
+  return(
+    <div onClick={clickCategory}>
+      {clicked ? <ClickedItem/> : <Item/>}
+      <ItemName>{category}</ItemName>
+    </div>
+  )
+}
+
+const ClickedItem = styled.div`
+  width: 80px;
+  height: 80px;
+  border-radius: 20px;
+  box-shadow: inset 5px 5px 10px #8f8f8f,
+    inset -5px -5px 10px #ffffff;
+`;
+
+const Item = styled.div`
+  width: 80px;
+  height: 80px;
+  border-radius: 20px;
+  box-shadow:  5px 5px 10px #8f8f8f,
+    -5px -5px 10px #ffffff;
+`;
+
+const ItemName = styled.p`
+  margin-top: 10px;
+  font-size: 13px;
+  color: ${(props) => props.theme.Gray3};
+  font-weight: bold;
+  text-align: center;
+`;
+
+export default CategoryItem;
--- client/src/pages/CreateGroupPage/Category/index.tsx
+++ client/src/pages/CreateGroupPage/Category/index.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from '@emotion/styled'
+import CategoryItem from './CategoryItem';
+
+const categoryList: string[] = [
+  '대외활동',
+  '공모전',
+  '스터디',
+  '동아리',
+  '면접/인터뷰',
+  '토이프로젝트',
+  '구인/구직',
+  '기타'
+];
 
 function Category(): JSX.Element {
+  const [selection, setSelection] = useState<string>('');
+  const getList = () => {
+    return categoryList.map((category,idx) => (
+      <CategoryItem
+        key={idx}
+        category={category}
+        clicked={category === selection}
+        clickCategory={() => { setSelection(category)} }
+      />)
+    )
+  }
   return(
-    <>Category Input</>
+    <>
+      <Title>카테고리를 선택해주세요.</Title>
+      <CategoryWrapper>
+        {getList()}
+      </CategoryWrapper>
+    </>
   )
 }
+
+const Title = styled.p`
+  margin: 30px;
+`;
+const CategoryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 40px;
+  margin: 0 70px;
+`;
 
 export default Category;
