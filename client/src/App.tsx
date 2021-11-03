944f4cd8888264ae1d200c083b5cff2b0253b22a
Donggyu Kim <ptsaturn68@daum.net>
2021-11-03 14:40:49 +0900

    [FE] feat: 헤더 메뉴 라우트 추가
    - 엔트리에 헤더 메뉴 라유트 추가
    
    - issue (#14)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { Header } from './components';
+import MainPage from './pages/MainPage';
+import GroupRecruitPage from './pages/GroupRecruitPage';
+import MentorRecruitPage from './pages/MentorRecruitPage';
+import GroupsPage from './pages/GroupsPage';
 
 function App(): JSX.Element {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <Switch>
+        <Route path="/" component={MainPage} exact />
+        <Route path="/recruit/group" component={GroupRecruitPage} exact />
+        <Route path="/recruit/mentor" component={MentorRecruitPage} exact />
+        <Route path="/groups" component={GroupsPage} exact />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
