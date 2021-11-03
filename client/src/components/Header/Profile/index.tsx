ccd13d4baf1a92cb8ba7f4a15c0e3aa33d71bd59
Donggyu Kim <ptsaturn68@daum.net>
2021-11-03 16:09:27 +0900

    [FE] design: 헤더 스타일 변경
    - 로고 사이즈 변경
    - default 프로필 이미지 등록
    
    - issue (#42)

--- client/src/components/Header/Profile/index.tsx
+++ client/src/components/Header/Profile/index.tsx
@@ -12,6 +12,9 @@ const Container = styled.div`
   border: 0.0625rem solid #f2f2f2;
   overflow: hidden;
   box-shadow: 0 2px 4px 0 hsl(0deg 0% 81% / 50%);
+  background-image: url(https://camo.githubusercontent.com/614e69129cbd90e364e863ad2de097121dd289e693e8b611a039dda08d7c3403/68747470733a2f2f692e696d6775722e636f6d2f665a346378737a2e706e67);
+  background-size: cover;
+  cursor: pointer;
 `;
 
 export default User;
