9049a5800782b46d4730d5ff5f2f563bf1241f64
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-04 16:29:30 +0900

    [FE] feat: GroupInfo Compoent 구현
    - 그룹 소개글 작성 textarea 적용

--- client/src/pages/CreateGroupPage/GroupInfo/index.tsx
+++ client/src/pages/CreateGroupPage/GroupInfo/index.tsx
@@ -1,9 +1,30 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
 function GroupInfo(): JSX.Element {
   return(
-    <>GroupInfo Input</>
+    <>
+      <Title>간단한 그룹소개를 해주세요.</Title>
+      <InfoText/>
+    </>
   )
 }
 
+const Title = styled.p`
+  margin: 30px;
+`;
+
+const InfoText = styled.textarea`
+  border: none;
+  padding: 20px;
+  width: 400px;
+  height: 150px;
+  resize: none;
+  border-radius: 10px;
+  box-shadow:  5px 5px 10px #8f8f8f,
+    -5px -5px 10px #ffffff;
+  &:focus{
+    outline: none;
+  }
+`
 export default GroupInfo;
