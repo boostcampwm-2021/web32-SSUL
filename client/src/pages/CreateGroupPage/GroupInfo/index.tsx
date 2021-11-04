93200fcc247c4aba2fe231ca1a0488408b13f392
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-04 16:55:31 +0900

    [FE] feat: GroupInfo Compoent 그룹 이름 추가
    - 그룹 이름 input바 추가
    - issue (#10)

--- client/src/pages/CreateGroupPage/GroupInfo/index.tsx
+++ client/src/pages/CreateGroupPage/GroupInfo/index.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 function GroupInfo(): JSX.Element {
   return(
     <>
+      <Title>그룹명을 작성해주세요.</Title>
+      <NameText/>
       <Title>간단한 그룹소개를 해주세요.</Title>
       <InfoText/>
     </>
@@ -11,7 +13,7 @@ function GroupInfo(): JSX.Element {
 }
 
 const Title = styled.p`
-  margin: 30px;
+  margin: 15px;
 `;
 
 const InfoText = styled.textarea`
@@ -19,6 +21,19 @@ const InfoText = styled.textarea`
   padding: 20px;
   width: 400px;
   height: 150px;
+  resize: none;
+  border-radius: 10px;
+  box-shadow:  5px 5px 10px #8f8f8f,
+    -5px -5px 10px #ffffff;
+  &:focus{
+    outline: none;
+  }
+`;
+const NameText = styled.input`
+  border: none;
+  padding: 20px;
+  width: 400px;
+  height: 50px;
   resize: none;
   border-radius: 10px;
   box-shadow:  5px 5px 10px #8f8f8f,
