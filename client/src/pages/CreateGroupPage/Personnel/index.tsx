93f5b4e2695920caceacd910fa2a37d0abe2827e
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-04 16:31:53 +0900

    [FE] feat: Personnel Component 구현
    - 레인지 슬라이더 구현
    - 현재 정원 수 화면 출력 구현
    - issue (#10)

--- client/src/pages/CreateGroupPage/Personnel/index.tsx
+++ client/src/pages/CreateGroupPage/Personnel/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import React, { useState } from 'react';
 
 function Personnel(): JSX.Element {
+  const [count, setCount] = useState<number>(1);
+  
+  const onChangeBar = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    setCount(value)
+  }
+  
   return(
-    <>Personnel Input</>
+    <>
+      <Title>그룹의 정원을 선택해주세요.</Title>
+      <RangeValue>{ count }</RangeValue>
+      <RangeBar type="range" min="1" max="30" onChange={onChangeBar} value={count}/>
+      
+    </>
   )
 }
 
+const Title = styled.p`
+  margin: 30px;
+`;
+
+const RangeBar = styled.input`
+  display: block;
+  -webkit-appearance: none;
+  background-color: ${(props) => props.theme.Gray6};
+  width: 300px;
+  height: 10px;
+  border-radius: 5px;
+  margin: 0 auto;
+  outline: 0;
+  
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      background-color: ${(props) => props.theme.Primary};
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      border: 2px solid white;
+      cursor: pointer;
+      transition: all .3s ease;
+  }
+`;
+
+const RangeValue = styled.p`
+  width: 300px;
+  line-height: 60px;
+  font-weight: bold;
+  font-size: 50px;
+  margin: 40px auto;
+  color: ${(props) => props.theme.Gray3};
+  text-shadow: white 2px 2px 2px;
+`;
 export default Personnel;
