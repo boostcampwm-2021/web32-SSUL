ce2a785f41c3aa30a78d45a37cd25552c0cf6f95
who-is-hu <dydgndlaek97@gmail.com>
2021-11-04 18:03:57 +0900

    [BE] feat: 내부 링크용 이동용 버튼 컴포넌트 작성

---
+++ client/src/components/LinkButton/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  width: number;
+  height: number;
+};
+
+const LinkButton = styled(Link)<Props>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 50px;
+  color: ${(props) => props.theme.White};
+  background-color: ${(props) => props.theme.Primary};
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  font-weight: 600;
+  border-radius: 8px;
+  cursor: pointer;
+`;
+
+export default LinkButton;
