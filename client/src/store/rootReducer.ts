82aabff7d6f018f868c9b840f1c88f1037ae0d5a
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-04 20:41:12 +0900

    [FE] feat: ReduxSlice 추가
    - CreateGroupSlice 구현 및 추가
    - interface type 추가
    - issue (#10)

--- client/src/store/rootReducer.ts
+++ client/src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import groupRecruit from './slices/groupRecruitFilterSlice';
+import createGroupInfo from './slices/createGroupInfo';
 
 const reducer = combineReducers({
   groupRecruit,
+  createGroupInfo
 });
 
 export type ReducerType = ReturnType<typeof reducer>;
---
+++ client/src/store/slices/createGroupInfo.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { GroupData } from '../../types/CreateGroup';
+
+export const createGroupInfoSlice = createSlice({
+  name: 'createGroupInfo',
+  initialState: {
+    category: '',
+    personnelCount: 1,
+    startDate: '',
+    endDate: '',
+    groupName: '',
+    groupInfo: '',
+    techStack: []
+  } as GroupData,
+  reducers:{
+    setGroupData(state, action) {
+      const newData = action.payload;
+      return {...state, ...newData};
+    },
+  },
+});
+
+export const { setGroupData } = createGroupInfoSlice.actions;
+export default createGroupInfoSlice.reducer;
