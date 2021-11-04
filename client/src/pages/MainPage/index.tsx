2fcc7699f14d70fd00364d7c97a6c12680da8a51
whoishu <dydgndlaek97@gmail.com>
2021-11-04 20:53:10 +0900

    [FE] feat: 카테고리 목록 API 와 연동
    - db에 가져온 카테고리 정보 사용

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import CategoryItem from './CategoryItem';
 import { CenterLayout } from '../../styles/global';
 import { LinkButton } from '../../components';
 import DeveloperImg from '../../assets/images/developer-img.jpg';
+import { getCategories } from '../../api/category';
+import { Category } from '../../types/Category';
 
 const Tab = Object.freeze({
   GROUP: 'GROUP',
@@ -12,6 +14,15 @@ const Tab = Object.freeze({
 
 function MainPage(): JSX.Element {
   const [tab, setTab] = useState(Tab.GROUP);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const categories = await getCategories();
+      setCategories(categories);
+    };
+    fetchCategories();
+  }, []);
 
   return (
     <CenterLayout>
@@ -29,8 +40,8 @@ function MainPage(): JSX.Element {
           <GroupFindContainer>
             <IntroText>함께 성장할 동료를 찾아보세요!</IntroText>
             <GroupCatagoryContainer>
-              {dummy.map(({ name, url }) => (
-                <CategoryItem key={name} name={name} url={url} />
+              {categories.map(({ id, name, imageUrl }) => (
+                <CategoryItem key={id} name={name} url={imageUrl} />
               ))}
             </GroupCatagoryContainer>
           </GroupFindContainer>
@@ -101,56 +112,5 @@ const DynamicImage = styled.img`
   height: 400px;
   object-fit: cover;
 `;
-
-const dummy = [
-  {
-    id: 1,
-    name: '대외활동',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Ffa5e9a6d-7bd4-4fb7-8c6d-11db223703d1%2Ficon-activity.png?table=block&id=43e802c2-b572-4766-baa9-2bba12acf767&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-  {
-    id: 2,
-    name: '공모전',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F1902d710-3265-4407-a921-c1a92c19cef8%2Ficon-competition.png?table=block&id=d1e8f407-06ac-4eb5-a49b-b318b9ff059b&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-  {
-    id: 3,
-    name: '스터디',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F075b46f8-ebd6-43ef-80b0-1cc1f2c6ebe0%2Ficon-study.png?table=block&id=45def0e6-f405-4184-93d8-ee69afa3ea23&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-  {
-    id: 4,
-    name: '동아리',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6bf20af7-65f2-42eb-ad01-40ab9bf27f2b%2Ficon-club.png?table=block&id=740f238e-743b-4d2c-959c-165d132de99c&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-  {
-    id: 5,
-    name: '면접',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F996a8113-fa0d-4e7b-ba37-c877e73cac3a%2Ficon-interview.png?table=block&id=401db8c1-6191-4ce5-91f6-8891aa72581c&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-  {
-    id: 6,
-    name: '프로젝트',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F05e1fee8-1d79-4b3a-97c3-02afdfa24032%2Ficon-project.png?table=block&id=b8f9ef6d-bc94-4888-9ea9-59cd8016a820&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-  {
-    id: 7,
-    name: '구인/구직',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fb78df422-e081-4a01-ac85-c93aec561bac%2Ficon-job.png?table=block&id=a1bdd29b-e96f-4a7c-957c-f2dfec6f82d5&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-  {
-    id: 8,
-    name: '기타',
-    url:
-      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F162b1684-f42a-4dbf-ada6-0015627bfa65%2Ficon-etc.png?table=block&id=5bc7f5d5-c603-45b1-ae84-a5a8c19ea7f9&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
-  },
-];
 
 export default MainPage;
