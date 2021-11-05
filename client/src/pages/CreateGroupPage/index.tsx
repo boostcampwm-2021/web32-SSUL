3e47703396665cd88bad682840c8dfcf44a73dad
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-05 19:27:58 +0900

    [FE] feat: 입력사항 체크 구현
    - 필수 입력사항 미기재 후 다음버튼 클릭시 알림메세지 출력 후 다음으로 이동 안되게 구현
    - 마지막Input인 경우 다음 -> 완료로 버튼 텍스트 변경
    - issue (#10)

--- client/src/pages/CreateGroupPage/index.tsx
+++ client/src/pages/CreateGroupPage/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Category from './Category';
 import Personnel from './Personnel';
 import TechStack from './TechStack';
@@ -7,38 +7,67 @@ import Date from './Date';
 import GageBar from './GageBar';
 import CustomButton from './CustomButton';
 import styled from '@emotion/styled';
+import { useSelector } from 'react-redux';
+import { ReducerType } from '../../store/rootReducer';
+import { GroupData } from '../../types/CreateGroup';
 
 const MAX_CONTENT_INDEX = 4;
 
 function CreateGroupPage(): JSX.Element {
   const [contentsNumber, setContentsNumber] = useState<number>(0);
-  
-  const getContents = () : JSX.Element | null =>{
-    switch (contentsNumber){
-      case 0: return <Category/>;
-      case 1: return <Personnel/>;
-      case 2: return <GroupInfo/>
-      case 3: return <Date/>;
-      case 4: return <TechStack/>;
-      default: return null;
+  const [notificationText, setNotificationText] = useState<string>('');
+  const groupData = useSelector<ReducerType, GroupData>((state) => state.createGroupInfo);
+
+  const getContents = (): JSX.Element | null => {
+    switch (contentsNumber) {
+      case 0:
+        return <Category />;
+      case 1:
+        return <Personnel />;
+      case 2:
+        return <GroupInfo />;
+      case 3:
+        return <Date />;
+      case 4:
+        return <TechStack />;
+      default:
+        return null;
     }
-  }
-  const clickPrevContents = () =>{
-    if(contentsNumber > 0)
-      setContentsNumber(contentsNumber-1);
-  }
-  
-  const clickNextContents = () =>{
-    if(contentsNumber < MAX_CONTENT_INDEX)
-      setContentsNumber(contentsNumber+1);
-  }
-  
+  };
+
+  const checkInput = () => {
+    switch (contentsNumber) {
+      case 0:
+        return groupData.category !== '';
+      case 2:
+        return groupData.groupName !== '' && groupData.groupInfo !== '';
+      case 3:
+        return groupData.startDate !== '' && groupData.endDate !== '';
+      case 4:
+        return <TechStack />;
+      default:
+        return true;
+    }
+  };
+  const clickPrevContents = () => {
+    if (contentsNumber > 0) setContentsNumber(contentsNumber - 1);
+  };
+
+  const clickNextContents = () => {
+    if (!checkInput()) {
+      setNotificationText('필수 입력사항을 입력해주세요!');
+      return;
+    }
+
+    setNotificationText('');
+    if (contentsNumber < MAX_CONTENT_INDEX) setContentsNumber(contentsNumber + 1);
+  };
+
   return (
     <CreateForm>
-      <GageBar contentsNumber={contentsNumber}/>
-      <ContentsContainer>
-        {getContents()}
-      </ContentsContainer>
+      <GageBar contentsNumber={contentsNumber} />
+      <ContentsContainer>{getContents()}</ContentsContainer>
+      <Notification>{notificationText}</Notification>
       <ButtonWrapper>
         <CustomButton
           label={'이전'}
@@ -47,14 +76,14 @@ function CreateGroupPage(): JSX.Element {
           clickBtn={clickPrevContents}
         />
         <CustomButton
-          label={'다음'}
+          label={contentsNumber === MAX_CONTENT_INDEX ? '완료' : '다음'}
           color={'#FFFFFF'}
           backgroundColor={'#00C5AA'}
           clickBtn={clickNextContents}
         />
       </ButtonWrapper>
     </CreateForm>
-  )
+  );
 }
 
 const CreateForm = styled.div`
@@ -63,7 +92,7 @@ const CreateForm = styled.div`
   margin: 70px auto 0 auto;
   width: 600px;
   height: 500px;
-  box-shadow: 20px 20px 40px 4px rgba(41, 36, 36, 0.25), -20px -20px 0px 6px #FFFFFF;
+  box-shadow: 20px 20px 40px 4px rgba(41, 36, 36, 0.25), -20px -20px 0px 6px #ffffff;
   border-radius: 40px;
 `;
 
@@ -79,6 +108,15 @@ const ButtonWrapper = styled.div`
   right: 0;
   margin-bottom: 40px;
   display: flex;
-  
-`
+`;
+
+const Notification = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  font-size: 13px;
+  margin-left: 40px;
+  margin-bottom: 40px;
+  color: #e50707;
+`;
 export default CreateGroupPage;
