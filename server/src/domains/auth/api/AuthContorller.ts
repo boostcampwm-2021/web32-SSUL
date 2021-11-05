46b1da80bf597e9507f5d6ef30ed73097e6431b6
Donggyu Kim <ptsaturn68@daum.net>
2021-11-05 23:41:16 +0900

    [BE] feat: 인증 session 처리
    - 유저 인증 정보를 session에 저장 완료
    - 현재 서버 메모리에 저장되어 있고 추후에 다른 방식으로 변경해야함
    
    - issue (#26)

--- server/src/domains/auth/api/AuthContorller.ts
+++ server/src/domains/auth/api/AuthContorller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, QueryParam } from 'routing-controllers';
+import { Controller, Get, QueryParam, Session, SessionParam } from 'routing-controllers';
 import { Inject, Service } from 'typedi';
 import { AuthService } from '../service/AuthService';
 
@@ -9,10 +9,15 @@ export class AuthController {
   private readonly authService: AuthService;
 
   @Get('/token')
-  async getAll(@QueryParam('code') code: string) {
+  async getGithubAccessToken(
+    @Session() session: any,
+    @SessionParam('githubId') githubId: string,
+    @QueryParam('code') code: string,
+  ) {
     const accessToken = await this.authService.getGithubAccessToken(code);
     const githubUserData = await this.authService.getGithubUserData(accessToken);
-    console.log(githubUserData);
+    if (!githubId) session.githubId = githubUserData.githubId;
+
     return githubUserData;
   }
 }
