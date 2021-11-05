34de49eed797c2498a867020b42e1fe8113efb29
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-06 04:06:27 +0900

    [FE] design: GroupCard Header 레이아웃 잡기
    - 그룹이름, 인원내역 추가
    
    - issue (#55)

--- client/src/components/GroupCard/GroupCardHeader/index.tsx
+++ client/src/components/GroupCard/GroupCardHeader/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-function GroupCardHeader(): JSX.Element {
-  return <Container></Container>;
+interface Props {
+  name: string | null;
+  maxUserCnt: number | null;
+  curUserCnt: number | null;
+}
+
+function GroupCardHeader({ name, maxUserCnt, curUserCnt }: Props): JSX.Element {
+  return (
+    <Container>
+      <h2>{name}</h2>
+      <UserCntStatus>
+        {curUserCnt}/{maxUserCnt}
+      </UserCntStatus>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const UserCntStatus = styled.div`
+  display: flex;
+  color: ${(props) => props.theme.Gray4};
 `;
 
 export default GroupCardHeader;
