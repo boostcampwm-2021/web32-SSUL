bc21da76bd2bc43b773a609784078483ba72849a
Donggyu Kim <ptsaturn68@daum.net>
2021-11-05 23:40:16 +0900

    [BE] feat: express-session 미들웨어 추가
    - config 설정 부분을 어떤 폴더로 배치해야할지 확신이 없음
    
    - issue (#26)

--- server/src/loader/express.ts
+++ server/src/loader/express.ts
@@ -1,5 +1,13 @@
 import * as express from 'express';
+import * as session from 'express-session';
+
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET as string,
+  resave: false,
+  saveUninitialized: true,
+};
 
 export default function (app: express.Application) {
   app.use(express.json());
+  app.use(session(sessionConfig));
 }
