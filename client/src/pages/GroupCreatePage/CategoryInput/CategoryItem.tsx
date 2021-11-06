e3100032fbce7e61f83fa008cb9267420c43656f
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-06 19:54:46 +0900

    [FE] feat: CategoryAPI Response값 적용
    - 기존 dummy데이터 -> Category정보를 getCategory API Response값 적용
    - 카테고리 이미지 적용 (imageURL)
    - CategoryItem: ClickedItem, UnclickedItem을 Item을 상속받는 styledComponent로 변경

--- client/src/pages/GroupCreatePage/CategoryInput/CategoryItem.tsx
+++ client/src/pages/GroupCreatePage/CategoryInput/CategoryItem.tsx
@@ -4,35 +4,42 @@ import styled from '@emotion/styled';
 interface ItemProp {
   key: number;
   category: string;
+  image: string;
   clicked: boolean;
   handleCategoryClick: () => void;
 }
 
-function CategoryItem({ category, clicked, handleCategoryClick }: ItemProp): JSX.Element {
+function CategoryItem({ category, clicked, image, handleCategoryClick }: ItemProp): JSX.Element {
   return (
     <div onClick={handleCategoryClick}>
-      {clicked ? <ClickedItem /> : <Item />}
+      {clicked ? (
+        <ClickedItem>
+          <CategoryIcon src={image} />
+        </ClickedItem>
+      ) : (
+        <UnClickedItem>
+          <CategoryIcon src={image} />
+        </UnClickedItem>
+      )}
       <ItemName>{category}</ItemName>
     </div>
   );
 }
-
-const ClickedItem = styled.div`
-  width: 60px;
-  height: 60px;
-  margin: auto;
-  border-radius: 10px;
-  box-shadow: inset 5px 5px 10px #8f8f8f, inset -5px -5px 10px #ffffff;
-`;
 
 const Item = styled.div`
   width: 60px;
   height: 60px;
   margin: auto;
   border-radius: 10px;
-  box-shadow: 5px 5px 10px #8f8f8f, -5px -5px 10px #ffffff;
 `;
 
+const ClickedItem = styled(Item)`
+  box-shadow: inset 5px 5px 10px #8f8f8f, inset -5px -5px 10px #ffffff;
+`;
+
+const UnClickedItem = styled(Item)`
+  box-shadow: 5px 5px 10px #8f8f8f, -5px -5px 10px #ffffff;
+`;
 const ItemName = styled.p`
   width: 80px;
   margin-top: 10px;
@@ -40,6 +47,13 @@ const ItemName = styled.p`
   color: ${(props) => props.theme.Gray3};
   font-weight: bold;
   text-align: center;
+`;
+
+const CategoryIcon = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-top: 5px;
+  margin-bottom: 8px;
 `;
 
 export default CategoryItem;
