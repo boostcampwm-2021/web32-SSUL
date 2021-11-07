d046e0b57b4344c106090214e36eec9e0ea1517d
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-07 18:30:04 +0900

    [FE] feat: Group API 파일 및 타입 생성
    - group API 파일 생성 및  axios 추상화 API함수 구현
    - POS요청에 필요한 CreateGroupInterface ServerData에 추가
    - issue (#20)

--- client/src/api/group.ts
+++ client/src/api/group.ts
@@ -1,4 +1,8 @@
 import { GroupResponse } from '@types';
+import { GroupCreateInterface } from '../types/ServerData';
 import { requests } from './index';
 
 export const getAllGroupList = (): Promise<GroupResponse> => requests.get(`/group`);
+
+export const postGroupCreate = <T>(groupData: GroupCreateInterface): Promise<T> =>
+  requests.post('/group/create', groupData);
