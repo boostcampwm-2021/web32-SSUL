2a75f8309973f01a2c0844d7b3b101fce2fd9851
Donggyu Kim <ptsaturn68@daum.net>
2021-11-07 21:50:40 +0900

    [FE] feat: 로더 컴포넌트 추가
    - issue (#27)

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as BubbleModal } from './BubbleModal';
 export { default as LinkButton } from './LinkButton';
 export { default as Auth } from './Auth';
+export { default as Loader } from './Loader';
