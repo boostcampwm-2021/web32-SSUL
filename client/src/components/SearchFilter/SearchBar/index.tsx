66719cbdd604cfb52c393fbfd4c6abe62336d7ac
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-07 19:34:09 +0900

    [FE] rename: SearchBar 위치 수정
    - SearchBar 폴더 형태로 수정

--- client/src/components/SearchFilter/SearchBar/index.tsx
+++ client/src/components/SearchFilter/SearchBar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { ReducerType } from '../../store/rootReducer';
+import { ReducerType } from '../../../store/rootReducer';
 import {
   changeTechStackInput,
   groupRecruitType,
