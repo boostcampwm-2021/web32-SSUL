0989006010cc9c7f0abf02320d8117ff64666f09
Donggyu Kim <ptsaturn68@daum.net>
2021-11-09 00:36:46 +0900

    [BE] refactor: destructObejct 함수 변경
    - key 변수를 let을 const로 변경
    - issue (#29)

--- server/src/utils/Object.ts
+++ server/src/utils/Object.ts
@@ -1,6 +1,6 @@
 export function destructObject(object: any): object {
   let obj = {};
-  for (let key in object) {
+  for (const key in object) {
     if (typeof object[key] === 'object') obj = { ...obj, ...destructObject(object[key]) };
     else obj = { ...obj, [key]: object[key] };
   }
