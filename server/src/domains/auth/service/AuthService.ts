433b609ee0f097bb11dfce5a67321809125aeb82
Donggyu Kim <ptsaturn68@daum.net>
2021-11-08 23:30:48 +0900

    [BE] feat: 유저 전역 정보 로직 추가
    - issue (#29)

--- server/src/domains/auth/service/AuthService.ts
+++ server/src/domains/auth/service/AuthService.ts
@@ -2,13 +2,18 @@ import { Service } from 'typedi';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import axios from 'axios';
 import { UserRepository } from '../../user/repository/UserRepository';
+import { ProfileRepository } from '../../user/repository/ProfileRepository';
 import { GithubUserDto } from '../Dto/AuthDto';
+import { UserDto } from '../../user/dto/UserDto';
+import { destructObject } from '../../../utils/Object';
 
 @Service()
 export class AuthService {
   constructor(
     @InjectRepository()
     private readonly userRepository: UserRepository,
+    @InjectRepository()
+    private readonly profileRepository: ProfileRepository,
   ) {}
 
   public async getGithubAccessToken(code: string) {
@@ -42,12 +47,15 @@ export class AuthService {
     return { githubId, name, avatarUrl };
   }
 
-  public async findOrInsertUser(user: GithubUserDto): Promise<GithubUserDto> {
-    const { githubId, name, avatarUrl } = user;
-    let userData = await this.userRepository.findOneById(githubId);
-    if (!userData) 
-      userData= await this.userRepository.insertUser(user);
-    
-    return userData as GithubUserDto;
+  public async findOrInsertUser(user: GithubUserDto): Promise<UserDto> {
+    const { githubId } = user;
+    let profileData = await this.profileRepository.findOneByUserId(githubId);
+    if (!profileData) {
+      const userData = await this.userRepository.insertUser(user);
+      profileData = await this.profileRepository.insertProfile(userData);
+    }
+
+    const userData = destructObject(profileData);
+    return userData as UserDto;
   }
 }
