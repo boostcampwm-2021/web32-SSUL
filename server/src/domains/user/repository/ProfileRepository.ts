57507d3d9a82feefdbfbca9258f4465a7e1e6b00
Donggyu Kim <ptsaturn68@daum.net>
2021-11-08 23:30:09 +0900

    [BE] feat: ProfileRepository 추가
    - issue (#29)

---
+++ server/src/domains/user/repository/ProfileRepository.ts
@@ -0,0 +1,35 @@
+import { Profile } from '../models/Profile';
+import { User } from '../models/User';
+import { Service } from 'typedi';
+import { Repository, EntityRepository } from 'typeorm';
+import { ProfileDto } from '../dto/ProfileDto';
+
+@Service()
+@EntityRepository(Profile)
+export class ProfileRepository extends Repository<Profile> {
+  public async insertProfile(user: User) {
+    const defaultProfile: ProfileDto = {
+      userId: user,
+      feverStack: 36.5,
+      shareStack: 0,
+      introduction: '',
+    };
+    await this.insert(defaultProfile);
+    return await this.findOneByUserId(user.githubId);
+  }
+  public async findOneByUserId(id: string) {
+    const profile = await this.createQueryBuilder('profile')
+      .select([
+        'user.id',
+        'user.githubId',
+        'user.name',
+        'user.avatarUrl',
+        'profile.feverStack',
+        'profile.shareStack',
+      ])
+      .leftJoin('profile.userId', 'user')
+      .getOne();
+
+    return profile;
+  }
+}
