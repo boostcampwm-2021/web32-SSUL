3e41f9691b1093fad58fb4f7632cd4ff2df5df24
Donggyu Kim <ptsaturn68@daum.net>
2021-11-09 00:34:09 +0900

    [FE] feat: getSlientRefresh 함수 추가
    - issue (#29)

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,5 +1,7 @@
 import { requests } from './index';
 import { ResponseGithubUserData } from '../types';
+export const getSlientRefresh = (): Promise<ResponseGithubUserData> =>
+  requests.get(`/auth/slient-refresh`);
 export const getAccessToken = (code: string): Promise<ResponseGithubUserData> =>
   requests.get(`/auth/token?code=${code}`);
 export const getLogout = (): Promise<null> => requests.get(`/auth/logout`);
