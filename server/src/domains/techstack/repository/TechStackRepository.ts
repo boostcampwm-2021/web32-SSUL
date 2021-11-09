c6bb9df605a0a27b0106dcf3489c2e8d6cb717af
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-09 15:43:05 +0900

    [BE] feat: findTechStackListByGroupId로 분할
    - groupId로 techStack 찾아오는거 repository에서 관리하게 변경
    - 다른 도메인에서도 불러서 사용이 가능
    
    - issue (#21)

--- server/src/domains/techstack/repository/TechStackRepository.ts
+++ server/src/domains/techstack/repository/TechStackRepository.ts
@@ -1,11 +1,21 @@
 import { TechStack } from '../models/TechStack';
+import { UsingTechStack } from '../models/UsingTechStack';
 import { Service } from 'typedi';
 import { Repository, EntityRepository } from 'typeorm';
 
 @Service()
 @EntityRepository(TechStack)
+@EntityRepository(UsingTechStack)
 export class TechStackRepository extends Repository<TechStack> {
   public findAll() {
     return this.find();
+  }
+  public async findTechStackListByGroupId(groupId: number) {
+    const techStackList = await this.createQueryBuilder('using_tech_stack')
+      .innerJoinAndSelect('using_tech_stack.techStack', 'tech_stack')
+      .where('using_tech_stack.type = :type', { type: 'GROUP' })
+      .andWhere('using_tech_stack.groupId = :groupId', { groupId })
+      .getMany();
+    return techStackList;
   }
 }
--- server/src/domains/techstack/service/TechStackService.ts
+++ server/src/domains/techstack/service/TechStackService.ts
@@ -1,28 +1,16 @@
 import { Service } from 'typedi';
 import { TechStackRepository } from '../repository/TechStackRepository';
-import { UsingTechStackRepository } from '../repository/UsingTechStackRepository';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 
 @Service()
 export class TechStackService {
   constructor(
     @InjectRepository()
-    private readonly usingTechStackRepository: UsingTechStackRepository,
-    @InjectRepository()
     private readonly techStackRepository: TechStackRepository,
   ) {}
 
   public async getTechStackList() {
     const techStackList = await this.techStackRepository.findAll();
-    return techStackList;
-  }
-
-  public async getGroupTechStackList() {
-    const techStackList = await this.usingTechStackRepository
-      .createQueryBuilder('using_tech_stack')
-      .innerJoinAndSelect('using_tech_stack.techStack', 'tech_stack')
-      .where('using_tech_stack.type = :type', { type: 'GROUP' })
-      .getMany();
     return techStackList;
   }
 }
