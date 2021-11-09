e0102cb8ff6b0d4485c7ca6a91ed13ab45c5960f
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-09 16:02:34 +0900

    [BE] feat: 기술 스택 배열 형태로 찾기
    - 각 그룹별 기술 스택 배열형태로 찾아서 반환
    
    - issue (#21)

--- server/src/domains/group/service/GroupService.ts
+++ server/src/domains/group/service/GroupService.ts
@@ -1,16 +1,27 @@
 import { Service } from 'typedi';
 import { GroupRepository } from '../repository/GroupRepository';
 import { InjectRepository } from 'typeorm-typedi-extensions';
+import { TechStackRepository } from '@domains/techstack/repository/TechStackRepository';
 
 @Service()
 export class GroupService {
   constructor(
     @InjectRepository()
     private readonly groupRepository: GroupRepository,
+    @InjectRepository()
+    private readonly techStackRepository: TechStackRepository,
   ) {}
 
   public async getGroups() {
     const groups = await this.groupRepository.findAll();
-    return groups;
+    const repackGroupInfo = await Promise.all(
+      groups.map(async (group) => {
+        const groupsTechStackList = await this.techStackRepository.findTechStackListByGroupId(
+          group.id,
+        );
+        return { ...group, teckStack: groupsTechStackList };
+      }),
+    );
+    return repackGroupInfo;
   }
 }
--- server/src/domains/techstack/repository/TechStackRepository.ts
+++ server/src/domains/techstack/repository/TechStackRepository.ts
@@ -2,6 +2,8 @@ import { TechStack } from '../models/TechStack';
 import { UsingTechStack } from '../models/UsingTechStack';
 import { Service } from 'typedi';
 import { Repository, EntityRepository } from 'typeorm';
+import { destructObject } from '@utils/Object';
+import { GroupTechStackListDto } from '../dto/groupTechStackListDto';
 
 @Service()
 @EntityRepository(TechStack)
@@ -11,11 +13,16 @@ export class TechStackRepository extends Repository<TechStack> {
     return this.find();
   }
   public async findTechStackListByGroupId(groupId: number) {
-    const techStackList = await this.createQueryBuilder('using_tech_stack')
+    const groupTechStackJoinResult = await this.createQueryBuilder('using_tech_stack')
       .innerJoinAndSelect('using_tech_stack.techStack', 'tech_stack')
       .where('using_tech_stack.type = :type', { type: 'GROUP' })
       .andWhere('using_tech_stack.groupId = :groupId', { groupId })
       .getMany();
-    return techStackList;
+
+    const techStackArray = groupTechStackJoinResult.map((techStack) => {
+      const { name } = destructObject(techStack) as GroupTechStackListDto;
+      return name;
+    });
+    return techStackArray;
   }
 }
