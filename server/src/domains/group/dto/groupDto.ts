d659413c2a7fdb8d8caf161dfc7928e2c745fce7
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-09 19:11:37 +0900

    [BE] style: 타입 nullable 설정
    - | null => ? 표현으로 변경

--- server/src/domains/group/dto/groupDto.ts
+++ server/src/domains/group/dto/groupDto.ts
@@ -8,11 +8,11 @@ export interface Group {
   id: number;
   mentorId: number;
   ownerId: number;
-  name: string | null;
-  maxUserCnt: number | null;
-  curUserCnt: number | null;
-  intro: string | null;
-  startAt: Date | null;
-  endAt: Date | null;
+  name?: string;
+  maxUserCnt?: number;
+  curUserCnt?: number;
+  intro?: string;
+  startAt?: Date;
+  endAt?: Date;
   status: GroupState;
 }
