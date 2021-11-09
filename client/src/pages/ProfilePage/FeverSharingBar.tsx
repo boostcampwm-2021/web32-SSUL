c5115245ed9adedfc9e40fc35e00420b4bc4227e
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-10 01:40:12 +0900

    [FE] design: 열정, 나눔지수 게이지바 애니메이션
    - left-> right 게이지가 차오르는 애니메이션 구현

--- client/src/pages/ProfilePage/FeverSharingBar.tsx
+++ client/src/pages/ProfilePage/FeverSharingBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
 
 function FeverSharingBar(): JSX.Element {
   return (
@@ -15,13 +16,24 @@ const Container = styled.div`
   max-width: 600px;
 `;
 
+const ProgressAnimation = keyframes`
+  from {
+    width: 0;
+  }
+  to {
+    width: 150px;
+  }
+`;
+
 const FeverIndex = styled.div`
   width: 150px;
   height: 20px;
   background-color: #ee7262;
   border-top-left-radius: 10px;
   border-bottom-left-radius: 10px;
+  animation: ${ProgressAnimation} 0.5s linear .1s normal none 1;
 `;
+
 
 const SharingIndex = styled.div`
   width: 150px;
@@ -29,5 +41,8 @@ const SharingIndex = styled.div`
   background-color: #9cde84;
   border-top-right-radius: 10px;
   border-bottom-right-radius: 10px;
+  animation: ${ProgressAnimation} 0.5s linear .1s normal none 1;
 `;
+
+
 export default FeverSharingBar;
