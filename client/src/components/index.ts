9aac4bce05ec97eb43df5855b87836988e0be0c2
Donggyu Kim <ptsaturn68@daum.net>
2021-11-09 19:24:41 +0900

    [FE] feat: 로딩 전역 관리를 위한 레이아웃 추가
    - 로딩 전역 관리를 위한 레이아웃 컴포넌트 추가
    
    - issue (#17)

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as DefaultLayout } from './Layout';
 export { default as Header } from './Header';
 export { default as BubbleModal } from './BubbleModal';
 export { default as LinkButton } from './LinkButton';
