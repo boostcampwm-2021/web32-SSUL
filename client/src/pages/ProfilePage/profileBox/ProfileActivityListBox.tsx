fafe4416000d260b3dbedb52aa2041969fa7984c
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-10 18:37:50 +0900

    [FE] feat: ProfileActivityListBox 컴포넌트 생성
    - ProfileActivityListBox: 활동내역  컴포넌트 
    - ProfileBox index.ts파일 생성

---
+++ client/src/pages/ProfilePage/profileBox/ProfileActivityListBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfileContainer from './ProfileBoxContainer';
+
+function ProfileActivityListBox(): JSX.Element {
+  return (
+    <>
+      <ProfileContainer title="활동내역">
+      </ProfileContainer>
+    </>
+  );
+}
+
+export default ProfileActivityListBox;
