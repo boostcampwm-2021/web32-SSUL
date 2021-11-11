e4579407d4369d50c41529ac50e066c159c28fd9
whoishu <dydgndlaek97@gmail.com>
2021-11-11 21:47:23 +0900

    [BE] chore: 카테고리 seed data 작성

---
+++ server/db_seed/category.ts
@@ -0,0 +1,50 @@
+export default [
+  {
+    category_id: 1,
+    name: '대외활동',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F05e1fee8-1d79-4b3a-97c3-02afdfa24032%2Ficon-project.png?table=block&id=b8f9ef6d-bc94-4888-9ea9-59cd8016a820&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+  {
+    category_id: 2,
+    name: '공모전',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F1902d710-3265-4407-a921-c1a92c19cef8%2Ficon-competition.png?table=block&id=d1e8f407-06ac-4eb5-a49b-b318b9ff059b&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+  {
+    category_id: 3,
+    name: '스터디',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F075b46f8-ebd6-43ef-80b0-1cc1f2c6ebe0%2Ficon-study.png?table=block&id=45def0e6-f405-4184-93d8-ee69afa3ea23&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+  {
+    category_id: 4,
+    name: '동아리',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6bf20af7-65f2-42eb-ad01-40ab9bf27f2b%2Ficon-club.png?table=block&id=740f238e-743b-4d2c-959c-165d132de99c&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+  {
+    category_id: 5,
+    name: '면접',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F996a8113-fa0d-4e7b-ba37-c877e73cac3a%2Ficon-interview.png?table=block&id=401db8c1-6191-4ce5-91f6-8891aa72581c&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+  {
+    category_id: 6,
+    name: '프로젝트',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F05e1fee8-1d79-4b3a-97c3-02afdfa24032%2Ficon-project.png?table=block&id=b8f9ef6d-bc94-4888-9ea9-59cd8016a820&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+  {
+    category_id: 7,
+    name: '구인/구직',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fb78df422-e081-4a01-ac85-c93aec561bac%2Ficon-job.png?table=block&id=a1bdd29b-e96f-4a7c-957c-f2dfec6f82d5&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+  {
+    category_id: 8,
+    name: '기타',
+    imageUrl:
+      'https://neighborly-ash-fed.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F162b1684-f42a-4dbf-ada6-0015627bfa65%2Ficon-etc.png?table=block&id=5bc7f5d5-c603-45b1-ae84-a5a8c19ea7f9&spaceId=1e190b35-e398-4c80-9518-9c3889034187&width=1020&userId=&cache=v2',
+  },
+];
