b988b69fc354385d65f17126fbe118beec981371
whoishu <dydgndlaek97@gmail.com>
2021-11-13 22:27:46 +0900

    [BE] chore: 응답 Dto API 문서 자동화 예시 추가

--- server/src/domains/category/models/Category.ts
+++ server/src/domains/category/models/Category.ts
@@ -1,13 +1,17 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNumber, IsString } from 'class-validator';
 
 @Entity('category')
 export class Category {
+  @IsNumber()
   @PrimaryGeneratedColumn({ name: 'category_id' })
   id: number;
 
+  @IsString()
   @Column('varchar', { name: 'name', nullable: true, length: 255 })
   name: string | null;
 
+  @IsString()
   @Column('varchar', { name: 'image_url', nullable: true, length: 500 })
   imageUrl: string | null;
 }
