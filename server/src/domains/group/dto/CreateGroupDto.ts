bdad885f0d9cebb7f3d34967b9e056a4201bdada
whoishu <dydgndlaek97@gmail.com>
2021-11-13 18:57:15 +0900

    [BE] chore: Dto에 class-validator 적용(GroupCreate)
    OpenAPI Schema로 확인 할 수 있게 끔 적용

--- server/src/domains/group/dto/CreateGroupDto.ts
+++ server/src/domains/group/dto/CreateGroupDto.ts
@@ -1,11 +1,22 @@
-export interface CreateGroupDto {
+import { IsArray, IsDate, IsNumber, IsString } from 'class-validator';
+
+export class CreateGroupDto {
+  @IsNumber()
   ownerId: number;
+  @IsString()
   name: string;
+  @IsNumber()
   maxUserCnt: number;
+  @IsNumber()
   curUserCnt: number;
+  @IsString()
   intro: string;
+  @IsDate()
   startAt: string;
+  @IsDate()
   endAt: string;
+  @IsString()
   category: string;
+  @IsArray()
   usingTechStacks: string[];
 }
