4e58455eea24e27955b2048e41ed21cde4c5c0be
whoishu <dydgndlaek97@gmail.com>
2021-11-15 01:43:32 +0900

    [BE] test: 카테고리 목록 api 테스트 코드
    간단한 예시로 성공 케이스만 작성

---
+++ server/test/domain/category/CategoryController.test.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import * as request from 'supertest';
+import appWrapper from '../../../src/app';
+
+describe('카테고리 api test', () => {
+  let app: express.Application;
+  beforeAll(async () => {
+    app = await appWrapper.getInstance();
+  });
+
+  describe('GET /category', () => {
+    test('카테고리 조회 성공', async () => {
+      const res = await request(app).get('/api/category');
+      expect(res.statusCode).toBe(200);
+    });
+  });
+});
