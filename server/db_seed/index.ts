7a8dcb70e01aa4e9b9b00f9508f156e01c4d3075
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-16 01:59:32 +0900

    [BE] chore: tech_stack 관련 DB seed 추가
    - tech_stack DB seed 추가
    - using_tech_stack DB seed 추가
    
    - issue (#108)

--- server/db_seed/index.ts
+++ server/db_seed/index.ts
@@ -1,2 +1,4 @@
 export { default as catagorySeedData } from './category.json';
 export { default as groupSeedData } from './group.json';
+export { default as techStackData } from './tech_stack.json';
+export { default as usingTechStackData } from './using_tech_stack.json';
--- server/src/utils/seed/index.ts
+++ server/src/utils/seed/index.ts
@@ -4,8 +4,10 @@ import { ormConfig } from '@config/ormconfig';
 
 import { Category } from '@domains/category/models/Category';
 import { Group } from '@domains/group/models/Group';
+import { TechStack } from '@domains/techstack/models/TechStack';
 
-import { catagorySeedData, groupSeedData } from '@root/db_seed';
+import { catagorySeedData, groupSeedData, techStackData, usingTechStackData } from '@root/db_seed';
+import { UsingTechStack } from '@domains/techstack/models/UsingTechStack';
 
 export async function seed() {
   const connection = await createConnection(ormConfig[config.mode]);
@@ -17,4 +19,11 @@ export async function seed() {
 async function seedDatabase(connection: Connection) {
   await connection.createQueryBuilder().insert().into(Category).values(catagorySeedData).execute();
   await connection.createQueryBuilder().insert().into(Group).values(groupSeedData).execute();
+  await connection.createQueryBuilder().insert().into(TechStack).values(techStackData).execute();
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(UsingTechStack)
+    .values(usingTechStackData)
+    .execute();
 }
