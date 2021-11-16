fed340d5b6319ffea8657e58d638b736eb4892ca
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-16 21:58:05 +0900

    [FE] style: Range util 네이밍 변경
    - 배열 Range 기능 이름 makeRangeNumberArray로 변경

--- client/src/utils/Range.ts
+++ client/src/utils/Range.ts
@@ -1,3 +1,3 @@
-export const rangeArray = (numCnt: number, start = 1): number[] => {
+export const makeRangeNumberArray = (numCnt: number, start = 1): number[] => {
   return [...Array.from(Array(numCnt).keys())].map((idx) => idx + start);
 };
