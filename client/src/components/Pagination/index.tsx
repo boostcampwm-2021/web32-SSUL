ba8c3a52de4cb1431c058087108d87e9670ac719
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-16 16:26:57 +0900

    [FE] design: pagination 페이지 UI 디자인
    - page 번호 UI 디자인
    
    - issue (#117)

--- client/src/components/Pagination/index.tsx
+++ client/src/components/Pagination/index.tsx
@@ -15,17 +15,39 @@ function Pagination({ totalPages, curPage }: PaginationProps): JSX.Element {
       ? totalPages
       : firstPageNumber + PAGE_OFFSET_CNT - 1;
   const pageCnt: number = lastPageNumber - firstPageNumber + 1;
-
   function range(numCnt: number, start = 1) {
     return [...Array.from(Array(numCnt).keys())].map((idx) => idx + start);
   }
 
   const renderPagination = range(pageCnt, firstPageNumber).map((pageNum: number) => {
-    return <div key={pageNum}>{pageNum}</div>;
+    if (pageNum === curPage)
+      return <SelectedPageNumber key={pageNum}>{pageNum}</SelectedPageNumber>;
+    else return <NonSelectedPageNumber key={pageNum}>{pageNum}</NonSelectedPageNumber>;
   });
 
   return <Container>{renderPagination}</Container>;
 }
+
+const PageNumber = styled.button`
+  width: 40px;
+  margin: 10px;
+  padding: 10px;
+  text-align: center;
+  box-shadow: 4px 4px 10px 0px rgba(41, 36, 36, 0.25);
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+`;
+
+const SelectedPageNumber = styled(PageNumber)`
+  color: ${(props) => props.theme.White};
+  background: ${(props) => props.theme.Primary};
+`;
+
+const NonSelectedPageNumber = styled(PageNumber)`
+  color: ${(props) => props.theme.Black};
+  background: ${(props) => props.theme.Gray5};
+`;
 
 const Container = styled.div`
   display: flex;
