d59d130009294b91b4219890653fbaa136f2734b
whoishu <dydgndlaek97@gmail.com>
2021-11-17 01:22:21 +0900

    [BE] feat: 에러 핸들러에서 logger 사용하여 로깅

--- server/src/common/error/handler/BadRequestErrorHandler.ts
+++ server/src/common/error/handler/BadRequestErrorHandler.ts
@@ -1,12 +1,14 @@
 import { BadRequestError, Middleware, ExpressErrorMiddlewareInterface } from 'routing-controllers';
 import { Service } from 'typedi';
 import { ErrorResponse } from '../ErrorResponse';
+import { logger } from '@utils/logger';
 
 @Service()
 @Middleware({ type: 'after' })
 export class BadRequestErrorHandler implements ExpressErrorMiddlewareInterface {
   error(error: any, request: any, response: any, next: (err: any) => any) {
     if (error instanceof BadRequestError) {
+      logger.error(error);
       console.error(error);
       response.status(400).json(ErrorResponse.fromBadRequestError(error));
     } else {
--- server/src/common/error/handler/ServerInternalErrorHandler.ts
+++ server/src/common/error/handler/ServerInternalErrorHandler.ts
@@ -1,11 +1,12 @@
 import { Middleware, ExpressErrorMiddlewareInterface } from 'routing-controllers';
 import { Service } from 'typedi';
+import { logger } from '@utils/logger';
 
 @Service()
 @Middleware({ type: 'after' })
 export class ServerInternalErrorHandler implements ExpressErrorMiddlewareInterface {
   error(error: any, request: any, response: any, next: (err: any) => any) {
-    console.error(error);
+    logger.error(error.message);
     response.status(500).json({
       status: 500,
       message: '관리자에게 문의해주세요',
