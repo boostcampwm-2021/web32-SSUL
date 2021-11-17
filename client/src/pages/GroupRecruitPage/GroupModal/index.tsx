48b1f3c70cb684ec00b2756fa1929a16e568d577
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-18 01:00:09 +0900

    [FE] feat: 그룹 상세보기 모달 틀 생성
    - 상세보기 모달 틀 구현
    
    - issue (#131)

--- client/src/pages/GroupRecruitPage/GroupModal/index.tsx
+++ client/src/pages/GroupRecruitPage/GroupModal/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { GroupDetail } from '@types';
+import GroupDetailHeader from './GroupDetailHeader';
+import GroupDetailStatus from './GroupDetailStatus';
 
 interface Props {
   contents: GroupDetail;
 }
 
 function GroupModal({ contents }: Props): JSX.Element {
-  return <Container>{JSON.stringify(contents)}</Container>;
+  const {
+    name,
+    curUserCnt,
+    maxUserCnt,
+    techStackList,
+    ownerName,
+    ownerAvatarUrl,
+    intro,
+    endAt,
+    startAt,
+  } = contents;
+
+  return (
+    <Container>
+      <h3>그룹 소개</h3>
+      <GroupDetailHeader {...{ name, curUserCnt, maxUserCnt }} />
+      <GroupDetailStatus {...{ techStackList, ownerName, ownerAvatarUrl, intro, endAt, startAt }} />
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+`;
 
 export default GroupModal;
