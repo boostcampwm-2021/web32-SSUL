3512b2ddb6a7434b29343975623d43addb2650f8
whoishu <dydgndlaek97@gmail.com>
2021-11-17 18:15:35 +0900

    [BE] refactor: ErrorSpec 의 코드값 number => string

--- server/src/common/error/ErrorCode.ts
+++ server/src/common/error/ErrorCode.ts
@@ -1,6 +1,7 @@
 import { ErrorSpec } from './ErrorSpec';
 
 export const ErrorCode = Object.freeze({
-  TEST_ERROR: new ErrorSpec(100, '테스트 에러 코드입니다.'),
-  BAD_REQUEST: new ErrorSpec(101, '사용자 입력 에러'),
+  TEST_ERROR: new ErrorSpec('T100', '테스트 에러 코드입니다.'),
+  BAD_REQUEST: new ErrorSpec('U100', '사용자 입력 에러'),
+  OAUTH_ERROR: new ErrorSpec('A200', '소셜로그인 인증 에러입니다.'),
 });
