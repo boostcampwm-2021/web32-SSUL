3c945d89095d2b707e8e5ff9429ad305e4bf1e36
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-17 18:43:02 +0900

    [FE] feat: group create floating 버튼
    - group 생성 플로팅 버튼 생성
    - routing 코드 수정
    
    - issue (#97)

--- client/src/pages/GroupRecruitPage/index.tsx
+++ client/src/pages/GroupRecruitPage/index.tsx
@@ -2,21 +2,62 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { SearchFilter } from '@components';
 import GroupCardList from './GroupCardList';
+import { Link } from 'react-router-dom';
 
 function GroupRecruitPage(): JSX.Element {
   return (
     <Container>
-      <SearchFilter />
-      <GroupCardList />
+      <SideLayout />
+      <Contents>
+        <SearchFilter />
+        <GroupCardList />
+      </Contents>
+      <SideLayout>
+        <FloatingButton to={{ pathname: `/group/create` }}>+</FloatingButton>
+      </SideLayout>
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
+  flex-direction: row;
+`;
+
+const SideLayout = styled.div`
+  display: flex;
+  width: 20%;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  width: 60%;
+
   justify-content: center;
   flex-direction: column;
+  min-width: 1000px;
   margin: 20px;
+`;
+
+const FloatingButton = styled(Link)`
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  bottom: 100px;
+  font-size: 40px;
+  margin: 10px;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  text-align: center;
+
+  box-shadow: 0 2px 4px 0 hsl(0deg 0% 81% / 50%);
+  color: ${(props) => props.theme.White};
+  background-color: ${(props) => props.theme.Primary};
+
+  &:hover {
+    color: ${(props) => props.theme.White};
+  }
 `;
 
 export default GroupRecruitPage;
