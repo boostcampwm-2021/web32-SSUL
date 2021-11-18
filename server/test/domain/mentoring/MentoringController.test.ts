150a5ad7069fa72dcad693da022b8ded1aa411b9
whoishu <dydgndlaek97@gmail.com>
2021-11-18 18:13:50 +0900

    [BE] test: 멘토 등록 api 통합 테스트

--- server/test/domain/mentoring/MentoringController.test.ts
+++ server/test/domain/mentoring/MentoringController.test.ts
@@ -1,8 +1,7 @@
-import { timeStamp } from 'console';
 import express from 'express';
-import { hasUncaughtExceptionCaptureCallback } from 'process';
 import request from 'supertest';
 import appWrapper from '../../../src/app';
+import { getLoginCookie } from '../../util/cookieSession';
 
 describe('/mentoring', () => {
   let app: express.Application;
@@ -25,6 +24,84 @@ describe('/mentoring', () => {
       const response = await request(app).get('/api/mentoring/mentor/2');
       //then
       expect(response.statusCode).toBe(400);
+    });
+  });
+
+  describe('[POST /mentor] 멘토 등록 신청', () => {
+    test('멘토 등록 성공', async () => {
+      //given
+      const cookieSession = getLoginCookie({
+        id: 3,
+      });
+      const mentorRegstDto = {
+        userId: 3,
+        techStacks: ['express', 'react'],
+      };
+
+      //when
+      const res = await request(app)
+        .post('/api/mentoring/mentor')
+        .set('Cookie', [cookieSession])
+        .send(mentorRegstDto);
+
+      //then
+      expect(res.statusCode).toBe(200);
+    });
+
+    test('본인이 아닐시 등록 실패', async () => {
+      //given
+      const cookieSession = getLoginCookie({
+        id: 2,
+      });
+      const mentorRegstDto = {
+        userId: 3,
+        techStacks: ['express', 'react'],
+      };
+
+      //when
+      const res = await request(app)
+        .post('/api/mentoring/mentor')
+        .set('Cookie', [cookieSession])
+        .send(mentorRegstDto);
+
+      //then
+      expect(res.statusCode).toBe(401);
+    });
+
+    test('기술 스택 없을때 400 에러', async () => {
+      //given
+      const cookieSession = getLoginCookie({ id: 2 });
+      const mentorRegstDto = {
+        userId: 2,
+        techStacks: [],
+      };
+
+      //when
+      const res = await request(app)
+        .post('/api/mentoring/mentor')
+        .set('Cookie', [cookieSession])
+        .send(mentorRegstDto);
+
+      //then
+      expect(res.statusCode).toBe(400);
+    });
+
+    test('기술 스택 5개초과 400 에러', async () => {
+      //given
+      const cookieSession = getLoginCookie({ id: 2 });
+      const mentorRegstDto = {
+        userId: 2,
+        techStacks: ['java', 'javascript', 'express', 'nginx', 'react', 'vue.js'],
+      };
+
+      //when
+      const res = await request(app)
+        .post('/api/mentoring/mentor')
+        .set('Cookie', [cookieSession])
+        .send(mentorRegstDto);
+
+      //then
+      expect(res.statusCode).toBe(400);
     });
   });
 });
