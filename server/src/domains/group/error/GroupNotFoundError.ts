86c4ad449a1c22d33ade37eb53618b3a6bc729d1
ChanYangYu <goodtss@soongsil.ac.kr>
2021-11-20 13:39:03 +0900

    [BE] rename: Error 이름 변경
    - invalid group id -> group not found

--- server/src/domains/group/error/GroupNotFoundError.ts
+++ server/src/domains/group/error/GroupNotFoundError.ts
@@ -1,8 +1,8 @@
 import { BusinessLogicError } from '@error/BusinessLogicError';
 import { ErrorCode } from '@error/ErrorCode';
 
-export class InvalidGroupIdError extends BusinessLogicError {
+export class GroupNotFoundError extends BusinessLogicError {
   constructor() {
-    super(400, `groupId is not correct`, ErrorCode.INVALID_GROUP_ID);
+    super(400, `groupId is not correct`, ErrorCode.GROUP_NOT_FOUND);
   }
 }
