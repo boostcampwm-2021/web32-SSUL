ff08f8b88cf725219a8a52dddcf4dd09c2008589
Donggyu Kim <ptsaturn68@daum.net>
2021-11-21 18:02:26 +0900

    [FE] feat: HttpClient 인터셉터 응답 에러 관련 로직 추가
    - issue (#145)

--- client/src/api/HttpClient.ts
+++ client/src/api/HttpClient.ts
@@ -5,18 +5,37 @@ interface AxiosDefaultConfig {
   baseURL?: string;
 }
 
+interface CustomError {
+  status: number;
+  code: string;
+  description: string;
+  error: AxiosError;
+}
+
 export default abstract class HttpClient {
   protected httpClient: AxiosInstance;
 
   public constructor(config: AxiosDefaultConfig) {
     this.httpClient = axios.create(config);
-    this.initResponseInterceptor();
+    this._initResponseInterceptor();
   }
 
-  private initResponseInterceptor = () => {
-    this.httpClient.interceptors.response.use(this.handleResponse, this.handleError);
+  private _initResponseInterceptor = () => {
+    this.httpClient.interceptors.response.use(this._handleResponse, this._handleError);
   };
-  private handleResponse = (response: AxiosResponse) => response.data;
+  private _handleResponse = (response: AxiosResponse) => response.data;
 
-  protected handleError = (error: AxiosError): Promise<never> => Promise.reject(error);
+  protected _handleError = (error: AxiosError): Promise<never> =>
+    Promise.reject(this._toCustomError(error));
+
+  private _toCustomError = (error: AxiosError): CustomError => {
+    const { status, errorSpec } = error.response?.data;
+    const responseError: CustomError = {
+      status,
+      code: errorSpec.code,
+      description: errorSpec.description,
+      error,
+    };
+    return responseError;
+  };
 }
