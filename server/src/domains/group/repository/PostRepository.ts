f789a709e06b54511dc2c8247d4ee94c91cfc56d
Donggyu Kim <ptsaturn68@daum.net>
2021-11-22 21:44:37 +0900

    [BE] refactor: PostRepository 주석 제거
    - issue (#112)

--- server/src/domains/group/repository/PostRepository.ts
+++ server/src/domains/group/repository/PostRepository.ts
@@ -4,8 +4,4 @@ import { Post } from '../models/Post';
 
 @Service()
 @EntityRepository(Post)
-export class PostRepository extends Repository<Post> {
-  // public async findAllByGroupId(groupId: number) {
-  //     return await this.fnid
-  // }
-}
+export class PostRepository extends Repository<Post> {}
