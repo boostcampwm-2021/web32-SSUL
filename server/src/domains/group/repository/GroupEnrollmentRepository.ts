d788858b462e56c407931ac4d7ec4e754cb08e39
whoishu <dydgndlaek97@gmail.com>
2021-11-23 13:05:56 +0900

    [BE] refactor: 그룹참여정보 찾는 함수 내부구현 감춤
    typeorm 종속적 코드를 내부 함수로 감춤

--- server/src/domains/group/repository/GroupEnrollmentRepository.ts
+++ server/src/domains/group/repository/GroupEnrollmentRepository.ts
@@ -4,4 +4,10 @@ import { GroupEnrollment } from '../models/GroupEnrollment';
 
 @Service()
 @EntityRepository(GroupEnrollment)
-export class GroupEnrollmentRepository extends Repository<GroupEnrollment> {}
+export class GroupEnrollmentRepository extends Repository<GroupEnrollment> {
+  findOneByGroupIdAndUserId(groupId: number, userId: number) {
+    return this.findOne({
+      where: { groupId, userId },
+    });
+  }
+}
