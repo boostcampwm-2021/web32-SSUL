daf8efd10d08fafbe7be40b07dd48c9cf8f2b1a1
Donggyu Kim <ptsaturn68@daum.net>
2021-11-24 15:24:46 +0900

    [FE] refactor: 가드 리팩토링
    - 주입 받는 형태로 변경

--- client/src/core/Router/routes/GroupBelongGuardRoute.tsx
+++ client/src/core/Router/routes/GroupBelongGuardRoute.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Route, RouteProps } from 'react-router-dom';
-import RouteGuard from '@core/Guard/RouteGaurd';
-import { RouteGuardScopes } from '@core/Guard/RouteGaurd';
+import { RouteGuard, authenficateReturnType } from '@core/Guard';
+import { authHttpClient } from '@api';
 
 function GroupBelongGuardRoute(props: RouteProps): JSX.Element {
   const component: RouteProps['component'] = props.component;
-  return <Route {...props} component={RouteGuard(component, RouteGuardScopes.GROUP_BELONG)} />;
+  return (
+    <Route
+      {...props}
+      component={RouteGuard(component, authHttpClient.isGroupBelong as authenficateReturnType)}
+    />
+  );
 }
 
 export default GroupBelongGuardRoute;
