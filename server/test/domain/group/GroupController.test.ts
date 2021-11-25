5a18ee3c2527948125a36fd6e393bee5f8475173
whoishu <dydgndlaek97@gmail.com>
2021-11-26 00:38:30 +0900

    [BE] test: 그룹 역할 가져오는 API
    지워진것 복원

--- server/test/domain/group/GroupController.test.ts
+++ server/test/domain/group/GroupController.test.ts
@@ -168,4 +168,53 @@ describe('그룹 컨트롤러', () => {
       expect(res.statusCode).toBe(400);
     });
   });
+
+  describe('[GET /role/:gid] 그룹에서의 역할 가져오기', () => {
+    test('그룹장 역할 가져오기 성공', async () => {
+      //given
+      const cookieSession = getLoginCookie({ id: 2 });
+
+      //when
+      const res = await request(app).get('/api/group/role/1').set('Cookie', [cookieSession]);
+
+      //then
+      expect(res.statusCode).toBe(200);
+      expect(res.body.type).toBe('OWNER');
+    });
+
+    test('멘토 역할 가져오기 성공', async () => {
+      //given
+      const cookieSession = getLoginCookie({ id: 1 });
+
+      //when
+      const res = await request(app).get('/api/group/role/1').set('Cookie', [cookieSession]);
+
+      //then
+      expect(res.statusCode).toBe(200);
+      expect(res.body.type).toBe('MENTOR');
+    });
+
+    test('멘티 역할 가져오기 성공', async () => {
+      //given
+      const cookieSession = getLoginCookie({ id: 4 });
+
+      //when
+      const res = await request(app).get('/api/group/role/1').set('Cookie', [cookieSession]);
+
+      //then
+      expect(res.statusCode).toBe(200);
+      expect(res.body.type).toBe('MENTEE');
+    });
+
+    test('현재 신청 중인 상태', async () => {
+      //given
+      const cookieSession = getLoginCookie({ id: 4 });
+
+      //when
+      const res = await request(app).get('/api/group/role/5').set('Cookie', [cookieSession]);
+
+      //then
+      expect(res.statusCode).toBe(400);
+    });
+  });
 });
