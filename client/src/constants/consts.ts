f6d92e14753dc9db21e38a5d2e0e2f41b6f0f8a8
Donggyu Kim <ptsaturn68@daum.net>
2021-11-27 00:27:51 +0900

    [FE] chore: constants 폴더 추가
    - issue (#152)

---
+++ client/src/constants/consts.ts
@@ -0,0 +1,7 @@
+// 메시지
+export const MSG_NOT_FOUND = '없는 페이지입니다';
+export const MSG_BAD_ACCESS = '좋지 못한 접근입니다.';
+
+// 디스크립션
+export const DESC_NOT_FOUND = '404 NOT FOUND';
+export const DESC_BAD_ACCESS = '4XX BAD ACCESS';
