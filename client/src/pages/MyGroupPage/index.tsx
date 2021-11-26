eafbe40166edf6cda29c75aa3a047ac00ff978f6
whoishu <dydgndlaek97@gmail.com>
2021-11-26 16:49:41 +0900

    [FE] feat: 그룹 참여 페이지 이름 변경
    GroupStatus > MyGroup

--- client/src/pages/MyGroupPage/index.tsx
+++ client/src/pages/MyGroupPage/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-function GroupStatusPage(): JSX.Element {
+function MyGroupPage(): JSX.Element {
   return <Container></Container>;
 }
 
 const Container = styled.div``;
 
-export default GroupStatusPage;
+export default MyGroupPage;
