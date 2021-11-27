bccb77da8d38eba6d5847c903b95bcc7fb2f66bc
Donggyu Kim <ptsaturn68@daum.net>
2021-11-27 17:51:27 +0900

    [BE] feat: Alarm 관련 Dto 추가
    - AlarmResponse
    - AlarmParams
    
    - issue (#207)

---
+++ server/src/domains/alarm/dto/AlarmResponse.ts
@@ -0,0 +1,37 @@
+import { IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
+import { Alarm } from '../models/Alarm';
+
+export class AlarmListResponse {
+  @IsNumber()
+  id: number | null;
+  @IsNumber()
+  senderId: number | null;
+  @IsString()
+  senderName: string | null;
+  @IsNumber()
+  recieverId: number | null;
+  @IsNumber()
+  groupId: number | null;
+  @IsString()
+  groupName: string | null;
+  @IsString()
+  type: string | null;
+  @IsDate()
+  createdAt: Date | null;
+  @IsBoolean()
+  readChk: boolean | null;
+
+  static from(alarm: Alarm) {
+    const dto = new AlarmListResponse();
+    dto.id = alarm.id;
+    dto.senderId = alarm.senderId;
+    dto.recieverId = alarm.recieverId;
+    dto.groupId = alarm.groupId;
+    dto.type = alarm.type;
+    dto.createdAt = alarm.createdAt;
+    dto.readChk = alarm.readChk;
+    dto.senderName = alarm.sender.name;
+    dto.groupName = alarm.group.name;
+    return dto;
+  }
+}
