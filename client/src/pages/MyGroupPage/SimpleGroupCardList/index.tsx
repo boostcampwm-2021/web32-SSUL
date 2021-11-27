3da208682b9426603774156097bda96502816690
whoishu <dydgndlaek97@gmail.com>
2021-11-27 17:24:36 +0900

    [FE] feat: SimpleGroupCardList 컴포넌트 작성

---
+++ client/src/pages/MyGroupPage/SimpleGroupCardList/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { SimpleGroupCardData } from '@types';
+import SimpleGroupCard from '../SimpleGroupCard';
+
+interface SimpleGroupCardListProps {
+  title: string;
+  groups: SimpleGroupCardData[];
+  isClickable: boolean;
+}
+
+function SimpleGroupCardList(props: SimpleGroupCardListProps): JSX.Element {
+  const { title, groups, isClickable } = props;
+  return (
+    <Container>
+      <CardListHeader>{title}</CardListHeader>
+      <ScrollConatiner>
+        {groups.map((group) => (
+          <SimpleGroupCard key={group.id} group={group} isClickable={isClickable} />
+        ))}
+      </ScrollConatiner>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 18%;
+  height: 100%;
+  padding: 16px 8px 16px 16px;
+  border-radius: 8px;
+  border: 1px solid ${(props) => props.theme.Gray4};
+  margin-right: 32px;
+  background-color: #f6f8fa;
+`;
+
+const CardListHeader = styled.span`
+  font-weight: 600;
+  font-size: 0.85rem;
+  margin-bottom: 30px;
+`;
+
+const ScrollConatiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+`;
+
+export default SimpleGroupCardList;
