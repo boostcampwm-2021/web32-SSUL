75e7f9c75c903101182cf0fd52651607e0373e69
yuchanleeme <yuchanlee.me@gmail.com>
2021-11-28 16:39:59 +0900

    [BE] refactor: MentoringRequest 레포 리팩토링
    - Mentor - findAllByName 리팩토링
    Co-authored-by: 김용후 <dydgndlaek97@gmail.com>
    Co-authored-by: 유찬양 <ChanYangYu@users.noreply.github.com>
    Co-authored-by: 김동규 <Donggggg@users.noreply.github.com>

--- server/src/domains/mentoring/repository/MentorRepository.ts
+++ server/src/domains/mentoring/repository/MentorRepository.ts
@@ -1,27 +1,19 @@
 import { Service } from 'typedi';
-import { Repository, EntityRepository } from 'typeorm';
+import { Repository, EntityRepository, Like } from 'typeorm';
 import { Mentor } from '../models/Mentor';
 
 @Service()
 @EntityRepository(Mentor)
 export class MentorRepository extends Repository<Mentor> {
   public findAllByName(name: string) {
-    return this.createQueryBuilder('mentor')
-      .innerJoin('mentor.techStacks', 'mentor_tech_stack')
-      .innerJoin('mentor.user', 'user')
-      .select(['mentor.id'])
-      .addSelect(['mentor_tech_stack.id', 'mentor_tech_stack.name'])
-      .addSelect([
-        'user.id',
-        'user.name',
-        'user.shareStack',
-        'user.avatarUrl',
-        'user.githubId',
-        'user.intro',
-        'user.createdAt',
-      ])
-      .where('user.name like :filterdName', { filterdName: `%${name}%` })
-      .getMany();
+    return this.find({
+      relations: ['techStacks', 'user'],
+      where: {
+        user: {
+          name: Like(`%${name}%`),
+        },
+      },
+    });
   }
 
   public findOneByUserId(userId: number) {
