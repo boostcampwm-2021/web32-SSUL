5a2087d62d4737e0cda5ada7c97e5dda7823df1f
Donggyu Kim <ptsaturn68@daum.net>
2021-12-02 04:10:01 +0900

    [FE] feat: 네비 메뉴 인증에 따른 노출 변경
    - issue (#239)

--- client/src/components/Header/Navigation/NavItemList.tsx
+++ client/src/components/Header/Navigation/NavItemList.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import NavItem from './NavItem';
+import { useAppSelector } from '@hooks';
+import { selectUser } from '@store/user/globalSlice';
 
 interface NavigationItem {
   title: string;
   path: string;
+  auth: boolean;
 }
 
+const navItemList: NavigationItem[] = [
+  { title: '그룹 찾기', path: '/recruit/group', auth: false },
+  { title: '멘토 찾기', path: '/recruit/mentor', auth: false },
+  { title: '참여 그룹', path: '/group/my', auth: true },
+];
+
 function NavItemList(): JSX.Element {
-  const navItemList: NavigationItem[] = [
-    { title: '그룹 탐색', path: '/recruit/group' },
-    { title: '멘토 찾기', path: '/recruit/mentor' },
-    { title: '참여 그룹', path: '/group/my' },
-  ];
+  const { isLogin } = useAppSelector(selectUser);
 
   const navItems = navItemList.map((item, idx) => {
-    return <NavItem title={item.title} path={item.path} key={idx} />;
+    if (isLogin || !item.auth) return <NavItem title={item.title} path={item.path} key={idx} />;
   });
 
   return <Container>{navItems}</Container>;
