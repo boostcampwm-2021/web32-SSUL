2676a0586db9adfde8b1366916b394b25e4e831f
whoishu <dydgndlaek97@gmail.com>
2021-12-17 22:32:35 +0900

    [BE] refactor: 포스트 삭제시 권한 검사 로직 추가
    바뀐 repo 함수 이름 적용

--- server/src/domains/post/service/PostService.ts
+++ server/src/domains/post/service/PostService.ts
@@ -4,6 +4,7 @@ import { PostRepository } from '../repository/PostRepository';
 import { Post } from '../models/Post';
 import { PostUpdateDto } from '../dto/PostUpdateDto';
 import { PostResponse } from '../dto/PostResponse';
+import { NotAuthorizedError } from '@common/error/NotAuthorizedError';
 
 @Service()
 export class PostService {
@@ -17,25 +18,27 @@ export class PostService {
     return posts.map((post) => PostResponse.from(post));
   }
 
-  public async createPost(post: Post): Promise<void> {
-    await this.postRepository.insert(post);
+  public async createPost(post: Post): Promise<Post> {
+    return await this.postRepository.save(post);
   }
 
-  public async updatePost(userId: number, post: PostUpdateDto): Promise<void> {
-    const { id: postId } = post;
-    await this.postRepository.findOneByPostIdOrFail(postId);
-    await this.postRepository.findOneByWriterDataOrFail(postId, userId);
-    await this.postRepository.updateByPostId(post);
+  public async updatePost(userId: number, postUpdateDto: PostUpdateDto): Promise<Post> {
+    const { id: postId } = postUpdateDto;
+    await this.postRepository.findOneByIdOrFail(postId);
+    await this.postRepository.findOneByIdAndUserIdOrFail(postId, userId);
+    return await this.postRepository.updateById(postId, postUpdateDto);
   }
 
   public async deletePost(userId: number, postId: number): Promise<void> {
-    await this.postRepository.findOneByPostIdOrFail(postId);
-    await this.postRepository.findOneByWriterDataOrFail(postId, userId);
-    await this.postRepository.deleteByPostId(postId);
+    const existPost = await this.postRepository.findOneByIdOrFail(postId);
+    if (existPost.userId !== userId) {
+      throw new NotAuthorizedError();
+    }
+    await this.postRepository.deleteById(existPost.id);
   }
 
   public async increaseHit(postId: number): Promise<void> {
-    const post = await this.postRepository.findOneByPostIdOrFail(postId);
+    const post = await this.postRepository.findOneByIdOrFail(postId);
     this.postRepository.increaseHit(post);
   }
 }
