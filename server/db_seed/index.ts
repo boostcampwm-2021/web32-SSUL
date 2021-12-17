e4c483521cf9a9488fbe9aa622a31b4c4b7b251e
whoishu <dydgndlaek97@gmail.com>
2021-12-17 20:28:00 +0900

    [BE] refactor: Post 관련 seed 기능 제거
    롤백 테스트로 변환하기 위해 삭제

--- server/db_seed/index.ts
+++ server/db_seed/index.ts
@@ -5,7 +5,6 @@ export { default as userSeedData } from './user.json';
 export { default as groupEnrollmentSeedData } from './group_enrollment.json';
 export { default as mentorSeedData } from './mentor.json';
 export { default as groupTechStackSeedData } from './group_tech_stack.json';
-export { default as postSeedData } from './post.json';
 export { default as applyGroupSeedData } from './apply_group.json';
 export { default as mentorTechStackSeedData } from './mentor_tech_stack.json';
 export { default as mentoringRequestSeedData } from './mentoring_request.json';
